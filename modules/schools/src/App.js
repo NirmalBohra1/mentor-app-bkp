b60a26ff44a4abe2c648e4cabe71f91f3a57c496
Nirmal <nirmalssbohra@gmail.com>
2022-07-09 17:07:40 +0530

    visit, school profile and report UI added

--- modules/schools/src/App.js
+++ modules/schools/src/App.js
@@ -6,10 +6,40 @@ import { extendTheme } from "native-base";
 import { DEFAULT_THEME, AppShell } from "@shiksha/common-lib";
 import SchoolProfile from "pages/SchoolProfile";
 import Myvisitspage from "./pages/MyVisitsPage";
+import AttendanceReportDashboard from "./pages/reports/AttendanceReportDashboard";
+import AttendanceSectionWiseReport from "./pages/reports/AttendanceSectionWiseReport";
+import AttendanceDetailedReport from "./pages/reports/AttendanceDetailedReport";
+import AssessmentReportDashboard from "./pages/reports/AssessmentReportDashboard";
+import AssessmentSectionWiseReport from "./pages/reports/AssessmentSectionWiseReport";
+import AssessmentDetailedReport from "./pages/reports/AssessmentDetailedReport";
 
 function App() {
   const theme = extendTheme(DEFAULT_THEME);
   const routes = [
+    {
+      path: "assessment-report",
+      component: AssessmentReportDashboard,
+    },
+    {
+      path: "assessment-section-report",
+      component: AssessmentSectionWiseReport,
+    },
+    {
+      path: "assessment-detailed-report",
+      component: AssessmentDetailedReport,
+    },
+    {
+      path: "attendance-report",
+      component: AttendanceReportDashboard,
+    },
+    {
+      path: "attendance-section-report",
+      component: AttendanceSectionWiseReport,
+    },
+    {
+      path: "attendance-detailed-report",
+      component: AttendanceDetailedReport,
+    },
     {
       path: "my-visits",
       component: Myvisitspage,
--- modules/schools/src/components/SchoolAdminDetailCard.js
+++ modules/schools/src/components/SchoolAdminDetailCard.js
@@ -14,6 +14,7 @@ import {
   IconByName,
   Collapsible,
 } from "@shiksha/common-lib";
+import SchoolAdminTile from "./SchoolAdminTile";
 function SchoolAdminDetailCard() {
   return (
     <Collapsible
@@ -26,67 +27,11 @@ function SchoolAdminDetailCard() {
     >
       <>
         <VStack space={0}>
-          <Box>
-            <Divider />
-            <Box py={4}>
-              <VStack space={6}>
-                <HStack alignItems="center" justifyContent="space-between">
-                  <Text>275 Students</Text>
-                  <Text>All Students</Text>
-                </HStack>
+          <Divider />
+          <SchoolAdminTile title={'275 Students'} />
 
-                <HStack alignItems="center">
-                  <HStack alignItems="center">
-                    <Box
-                      bg="#3498DB"
-                      style={{ width: "12px", height: "12px" }}
-                    ></Box>
-                    <Text mx={2}>Boys:</Text>
-                    <Text bold>150</Text>
-                  </HStack>
-                  <HStack ml={4} alignItems="center">
-                    <Box
-                      bg="#BB6BD9"
-                      style={{ width: "12px", height: "12px" }}
-                    ></Box>
-                    <Text mx={2}>Girls:</Text>
-                    <Text bold>125</Text>
-                  </HStack>
-                </HStack>
-              </VStack>
-            </Box>
-          </Box>
-
-          <Box>
-            <Divider />
-            <Box py={4}>
-              <VStack space={6}>
-                <HStack alignItems="center" justifyContent="space-between">
-                  <Text>08 Teachers</Text>
-                  <Text>All Teachers</Text>
-                </HStack>
-
-                <HStack alignItems="center">
-                  <HStack alignItems="center">
-                    <Box
-                      bg="#3498DB"
-                      style={{ width: "12px", height: "12px" }}
-                    ></Box>
-                    <Text mx={2}>Male: </Text>
-                    <Text bold>05</Text>
-                  </HStack>
-                  <HStack ml={4} alignItems="center">
-                    <Box
-                      bg="#BB6BD9"
-                      style={{ width: "12px", height: "12px" }}
-                    ></Box>
-                    <Text mx={2}>Female: </Text>
-                    <Text bold>03</Text>
-                  </HStack>
-                </HStack>
-              </VStack>
-            </Box>
-          </Box>
+          <Divider />
+          <SchoolAdminTile title={'08 Teachers'} />
         </VStack>
       </>
     </Collapsible>
